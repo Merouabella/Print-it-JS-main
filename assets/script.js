0e2751b477176fdacc834b501930d7fa408ad118
Merouabella <131451877+Merouabella@users.noreply.github.com>
2023-12-03 12:46:33 +0100

    Écouteurs d'événements pour les bullet points

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,7 @@ const slides = [
         "image": "slide4.png",
         "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
     },
+	
 ];
 
 // Définir l'indice de la diapositive actuelle
@@ -37,24 +38,51 @@ flechDroite.addEventListener("click" ,  function()  {ChangeSlide(1);
 });
 console.log(flechDroite);
 
+
+// ...
+
+// ...
+
 // Slides
+function ChangeSlide(sens) {
+    numero = numero + sens;
 
-function ChangeSlide(sens){
-	numero = numero + sens;
-	if(numero > slides.length - 1)
-	numero = 0;
-if (numero < 0)
-numero =slides.length - 1;
+    // Si on est à la dernière image et que l’on clique à droite
+    if (numero > slides.length - 1) {
+        numero = 0;
+    }
 
+    // Si on est à la première image et qu’on clique à gauche
+    if (numero < 0) {
+        numero = slides.length - 1;
+    }
 
+    // Recuperation des images et textes
+    document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[numero]['image'];
+    document.getElementById("text").innerHTML = slides[numero]['tagLine'];
 
-// Recuperation des images et textes
-
-document.querySelector(".banner-img").src ='./assets/images/slideshow/' + slides[numero] ['image'];
-document.getElementById("text").innerHTML = slides[numero]['tagLine'];
-{console.log(sens)}
+    // Mettre à jour les bullets points
+    updateBulletPoints();
 }
+
+
+    // Changer la couleur du point actif
+    const currentDot = document.getElementById(`dot-${numero}`);
+    currentDot.classList.add('active');
+
+
+
 // Affichage des bullets points.
 
-const 
-    
+
+
+// Écouteurs d'événements pour les bullet points
+for (let i = 0; i < slides.length; i++) {
+    const bullet = document.getElementById(`bullet-${i}`);
+    bullet.addEventListener("click", function () {
+        ChangeSlide(i - numero); // Déplacement relatif pour naviguer directement
+    });
+}
+
+
+
